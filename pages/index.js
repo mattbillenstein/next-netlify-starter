6552442ba8120d20ebb3a4a4c3c4f18e1b7fa654
Matt Billenstein <matt@vazor.com>
2023-03-30 05:39:50 -0700

    Log some stuff

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  console.log("Rendered home");
+  console.log("Rendered home2");
   return (
     <div className="container">
       <Head>
